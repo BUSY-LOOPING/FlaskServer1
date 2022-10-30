690ac239e3d602b4509b109f20e8ea4a3bb4b616
BUSY-LOOPING <dhruvyadav2905@gmail.com>
2022-10-30 18:47:44 +0530

    First Commit

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api', methods = ['GET'])
+def return_ascii() :
+    dct = {}
+    inputChar = str(request.args['query'])
+    ans = str(ord(inputChar))
+    dct['output'] = ans
+    return dct
+
+if __name__ == "__main__" :
+    app.run(host='0.0.0.0', port=5000)
